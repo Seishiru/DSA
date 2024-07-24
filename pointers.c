5690ea4aa21667fc9e0b939508cb4f297fa92a8c
Kotegawa <143763801+Seishiru@users.noreply.github.com>
2024-07-24 18:24:04 +0800

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+    int **c, *b, a= 27;
+    *b = a;
+    c = &b;
+
+    printf("value of a: %d\n", a);
+    printf("address of a: %p\n\n", &a);
+
+    printf("b is holding &a: %p\n", b);
+    printf("b is getting the value of a: %d\n", *b);
+    printf("address of b: %p\n\n", &b);
+
+    printf("c is holding &b %p\n", c);
+    printf("c is getting the value of b where b is pointing: %d\n", **c);
+    printf("address of c%p\n", &c);
+    
+}
